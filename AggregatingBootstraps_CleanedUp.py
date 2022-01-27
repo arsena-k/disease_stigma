4ca281cde153abfd478bca35cdce8adbe7fe7fcd
Alina Arseniev-Koehler <arsena@g.ucla.edu>
2022-01-27 13:08:38 -0800

    adding col names

--- AggregatingBootstraps_CleanedUp.py
+++ AggregatingBootstraps_CleanedUp.py
@@ -7,55 +7,66 @@ Created on Fri Jan  8 13:33:24 2021
 
 import pandas as pd
 import os
-import numpy as np
-import matplotlib.pyplot as plt
 
 os.chdir('C:/Users/arsen/Dropbox/R01DiseaseStigma/Analyses/') 
 
 
-######################################
+    
+dim= 'med' #manually change to desired dimension (disgust, impurity, danger, or negpostraits) or to med for medicalization
 
-dim= 'danger' #change this to the dimension of interest
+# FIRST AGGREGATE BOOTSTRAPS FROM TIME PERIOD:
 
-diseases= pd.read_csv('temp' + str(dim) + '1980.csv')
-diseases1= pd.read_csv('temp' + str(dim) +'1983.csv')
+    
+diseases= pd.read_csv('temp' + str(dim) + '1980.csv')#had to save as csv in utf8
+diseases1= pd.read_csv('temp' + str(dim) +'1983.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp' + str(dim) + '1986.csv')
+diseases1= pd.read_csv('temp' + str(dim) + '1986.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp' + str(dim) + '1989.csv')
+diseases1= pd.read_csv('temp' + str(dim) + '1989.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp' + str(dim) + '1992.csv')
+diseases1= pd.read_csv('temp' + str(dim) + '1992.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp' + str(dim) + '1995.csv')
+diseases1= pd.read_csv('temp' + str(dim) + '1995.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp' + str(dim) + '1998.csv')
+diseases1= pd.read_csv('temp' + str(dim) + '1998.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp'+ str(dim) +'2001.csv')
+diseases1= pd.read_csv('temp'+ str(dim) +'2001.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp'+ str(dim) +'2004.csv')
+diseases1= pd.read_csv('temp'+ str(dim) +'2004.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp'+ str(dim) +'2007.csv')
+diseases1= pd.read_csv('temp'+ str(dim) +'2007.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp'+ str(dim) +'2010.csv')
+diseases1= pd.read_csv('temp'+ str(dim) +'2010.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp'+ str(dim) +'2013.csv')
+diseases1= pd.read_csv('temp'+ str(dim) +'2013.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
-diseases1= pd.read_csv('temp'+ str(dim) +'2016.csv')
+diseases1= pd.read_csv('temp'+ str(dim) +'2016.csv')#had to save as csv in utf8
 diseases= diseases.append(diseases1)
 
 
-# AGGREGATE AND COMPUTE RANK ORDER OF SCORES TO GET CONFIDENCE INTERVALS FOR THIS DIMENSION
+# SECOND, AGGREGATE BOOTSTRAPPED SCORES, AND CLEAN UP
 
 
 grouped = diseases.groupby(['Reconciled_Name', 'Year'])
-aggregated= grouped.describe(percentiles=[.04, .5, .96]) 
+
+aggregated= grouped.describe(percentiles=[.04, .5, .96]) #92 percent CI
 aggregated = aggregated.reset_index()
-aggregated2= aggregated.merge(diseases, how= 'left', on=['Reconciled_Name', 'Year']) 
-aggregated2= aggregated2.drop_duplicates(subset=['Reconciled_Name', 'Year'])
+aggregated2= aggregated.merge(diseases, how= 'left', on=['Reconciled_Name', 'Year']) #merge back in  
+aggregated2 = aggregated2.drop_duplicates(subset=['Reconciled_Name', 'Year'])
+aggregated2 = aggregated2.drop(columns=aggregated2.columns[[2,3,4,5,6,7,8,9,10, 11, 20, -2, -1]])
+
+aggregated2.columns = ['Reconciled_Name', 'Year', "count",	"mean"	, "std"	 ,"min"	, "CI4%",	"CI50%"	,"CI96%"	,"max",	"PlottingGroup"]
+aggregated2['Dimension']= str(dim)
 
 
-# SAVE FINAL CSV WITH AGGREGATED SCORES FOR THIS DIMENSION
+# THIRD, WRITE RESULTS TO CSV
+
 aggregated2.to_csv('C:/Users/arsen/Dropbox/R01DiseaseStigma/RESULTS/LexisNexis_News/'+ str(dim)+ '_aggregated_temp_92CI.csv') 
     
+
+
+
+
+
 
 
